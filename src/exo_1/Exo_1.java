742fc25031b4cd6f8cb5e8a6b045352f3211377b
delig <delig@LAPTOP-OJQO6C9O>
2021-10-08 12:45:45 +0200

    création du dépot

---
+++ src/exo_1/Exo_1.java
@@ -0,0 +1,30 @@
+/*
+ tp0
+deligne Tahina
+08/10
+*/
+package exo_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author delig
+ */
+public class Exo_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("bonjour");
+        String prenom;
+        Scanner sc;
+        sc= new Scanner(System.in);
+        System.out.println("Bonjour quel est votre prénom");
+        prenom=sc.nextLine();
+        System.out.println("au revoir");
+
+    }
+    
+}
